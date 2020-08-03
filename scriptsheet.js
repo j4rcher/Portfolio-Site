5734d8aa9b99794322c470cafa03ee100e187818
j4rcher <huxp.ja@gmail.com>
2020-08-03 17:30:06 +0100

    added contact menu for mobile w/ fade-in etc

--- scriptsheet.js
+++ scriptsheet.js
@@ -2,6 +2,14 @@ function changeVis() {
   document.querySelector(".fade-test").classList.toggle("visible");
 }
 
+function openContactMenu() {
+  document.querySelector(".contact-menu").style.animation = "fade-in 0.5s forwards";
+}
+
+function closeContactMenu() {
+  document.querySelector(".contact-menu").style.animation = "fade-out 0.5s forwards";
+}
+
 var isInViewport = function (elem) {
     var bounding = elem.getBoundingClientRect();
     return (
